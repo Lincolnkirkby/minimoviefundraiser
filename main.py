9563a70dfcb292f200e05bf9e7bdc41f7089883c
Lincolnkirkby <101753274+Lincolnkirkby@users.noreply.github.com>
2023-05-15 00:17:02 +0000

    last periods work

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import random
+import random #random number required for raffle
+#variables
 ticketssold = 0
-maxtickets = 4
+maxtickets = 5
 profit = 0
+totalticketsales = 0
 namelist = [""]
 costlist = [""]
 surchargelist = [""]
-winner = ""
 printnumber = 1
+winner = ""
+
+#yes/no checker
 def yesnofunc():
   yesno = input("do you wish to see program instructions? ")
   if yesno =="yes" or yesno == "y":
-    print("instructions go here")
+    print("instructions go here")#placeholder
   elif yesno == "no" or yesno == "n":
     pass
   else:
@@ -18,6 +22,7 @@ def yesnofunc():
     yesnofunc()
 yesnofunc()
 
+#while loop
 while ticketssold < maxtickets:
   ticketcost = 0
   surcharge = 0
@@ -31,6 +36,7 @@ while ticketssold < maxtickets:
     else:
       print("you need to have sold at least one ticket first")
       continue
+      
   age = input("age: ")
   try: 
     int(age)
@@ -46,50 +52,44 @@ while ticketssold < maxtickets:
   else: 
     print("that looks like a typo, please try again")
     continue
+  if age <16:
+    ticketcost = 7.50
+  elif age <65:
+    ticketcost =10.50
+  else: 
+    ticketcost = 6.50
+    
   payment = input("cash or credit?: ").lower()
   if payment == "cash" or payment == "ca":
-    if age <16:
-      profit +=2.50
-      surcharge += 0
-      ticketcost += 7.50
-    elif age <65:
-      profit +=5.50
-      surcharge+=0
-      ticketcost +=10.50
-    else: 
-      profit += 1.50
-      surcharge += 0
-      ticketcost += 6.50
+    pass
   elif payment == "credit" or payment == "cr":
-    if age <16:
-      profit +=2.50
-      surcharge += 0.38
-      ticketcost += 7.50
-    elif age <65:
-      profit +=5.50
-      surcharge+=1.03
-      ticketcost +=10.50
-    else: 
-      profit += 1.50
-      surcharge += 0.33
-      ticketcost += 6.50
+    surcharge = round(ticketcost*0.05, 2)
   else:     
-    print("error, only aceptable inputs are \"cash\" or \"credit\" ")
+    print("error, only aceptable inputs are \"cash\" or \"credit\", \"ca\" or \"cr\"")
     continue
+    
+  profit += (ticketcost-5)
+  totalticketsales += ticketcost + surcharge
   namelist.append(name)
   costlist.append(ticketcost)
   surchargelist.append(surcharge)
   ticketssold +=1
+
+
 if ticketssold == maxtickets: print("\ncongrats on selling all tickets")
 else: print("\ncongrats, you have sold",ticketssold,"ticket(s)")
-print("\n                  ---ticket data---\n")
 
+print("\n-------Ticket Data-------\n")
 while printnumber < (ticketssold+1):
-  print("ticket #"+str(printnumber)+", name:",str(namelist[printnumber])+", ticket cost:",str(costlist[printnumber])+", surcharge:",str(surchargelist[printnumber]),"\n")
+  print("ticket #"+str(printnumber)+", name:",str(namelist[printnumber])+", ticket cost: $"+str(costlist[printnumber])+", surcharge: $"+str(surchargelist[printnumber]),"\n")
   printnumber += 1
 
+print("-------Ticket Cost / Profit-------\n")
+print("total profits: $"+str(profit))
+print("total ticket sales: $"+str(totalticketsales))
 
+
+print("\n-------Raffle Winner-------\n")
 winner = random.randint(1,ticketssold)
 print("the winner is",namelist[winner]+",",namelist[winner],"has won $"+str(costlist[winner]),"A.K.A",namelist[winner],"has won a free ticket")
-print("total profits: $"+str(profit))
 quit()
